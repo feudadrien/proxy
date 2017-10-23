1d8151f418c3a25ae0f52cafdbd37307060ed620
feudadrien <adfeu1@morgan.edu>
2017-10-23 03:14:34 -0400

    Routing Link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, RouterLinkActive } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CenterComponent } from './center/center.component';
+import { RequestComponent } from './request/request.component';
+import { ApproveComponent } from './approve/approve.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    CenterComponent
+    CenterComponent,
+    RequestComponent,
+    ApproveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+      path: 'center',
+      component: CenterComponent
+    },
+    {
+      path: 'request',
+      component: RequestComponent
+    },
+    {
+      path: 'approve',
+      component: ApproveComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
